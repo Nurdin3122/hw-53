57e5014ac9f7a91ca557f529ea20bbeb7c6f00ea
Muhtarov Nurdin <nurdinmuhtarovv@gmail.com>
2024-05-24 17:35:59 +0600

    Добавил обработчик события удаления элемента и передал  его в компонент Task.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import React ,{useState}  from 'react';
 import AddTaskForm from "./AddTaskForm/AddTaskForm.tsx";
+import Task from "./Task/Task.tsx";
 
 interface Task {
     id: string;
@@ -15,19 +16,30 @@ const App: React.FC = () => {
         setTasks((prevState) => {
            const tasksCopy = [...prevState];
             tasksCopy.push(newText)
-            console.log(tasksCopy)
             return tasksCopy
-
         } )
+    };
 
+    const deleteTask = (id: string) => {
+        setTasks(tasks.filter(task => task.id !== id));
     };
 
 
     return (
-        <div className="app">
-            <AddTaskForm onSubmit={createText} />
-        </div>
+        <>
+            <div className="app">
+                <AddTaskForm onSubmit={createText}/>
+
+                <div>
+                    {tasks.map(task => (
+                        <Task key={task.id} id={task.id} text={task.text} deleteTask={deleteTask} />
+                    ))}
+                </div>
+            </div>
+
+
+        </>
+
     );
 };
-
 export default App;
